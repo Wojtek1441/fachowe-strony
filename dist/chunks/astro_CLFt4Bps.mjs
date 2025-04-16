a46fb79adfc89e22a67ac2e8da99ec7d1b30e736
Wojtek1441 <motyl.motyl@interia.pl>
2025-04-16 17:47:51 +0000

    dist-no blog2

--- dist/chunks/astro_CLFt4Bps.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_DoRQTvF3.mjs';
-import 'clsx';
--- dist/chunks/jak-stworzyc-skuteczne-strony-internetowe_DD6sw57w.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./jak-stworzyc-skuteczne-strony-internetowe_Dru0aUQX.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
